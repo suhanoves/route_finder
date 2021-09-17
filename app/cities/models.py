55740d1aea160d74afef90ed770fa42f7cc691be
Eugene Suhanov <suhanoves@gmail.com>
2021-09-17 21:08:40 +0300

    Add get_absolute_url for City model

--- app/cities/models.py
+++ app/cities/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class City(models.Model):
@@ -11,3 +12,6 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('cities:city', kwargs={'pk':self.pk})
