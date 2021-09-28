5ae1ebcc59a654b9937024bc95161d2b21f0dd7f
Eugene Suhanov <suhanoves@gmail.com>
2021-09-28 19:37:53 +0300

    add constraint for model Flight

--- app/flights/models.py
+++ app/flights/models.py
@@ -1,4 +1,10 @@
+from datetime import timedelta
+from decimal import Decimal
+
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import F, Q
 from django.urls import reverse
 
 from cities.models import City
@@ -12,11 +18,17 @@ class Flight(models.Model):
     )
     duration = models.DurationField(
         verbose_name='длительность',
+        validators=[
+            MinValueValidator(timedelta(minutes=1)),
+        ]
     )
     price = models.DecimalField(
         verbose_name='стоимость',
         max_digits=14,
         decimal_places=2,
+        validators=[
+            MinValueValidator(Decimal('0.01')),
+        ]
     )
     origin = models.ForeignKey(
         City,
@@ -36,6 +48,53 @@ class Flight(models.Model):
     class Meta:
         verbose_name = 'рейс'
         verbose_name_plural = 'рейсы'
+
+        constraints = (
+            models.UniqueConstraint(
+                fields=('origin', 'destination', 'duration', 'price'),
+                name='origin_destination_duration_price_unique',
+            ),
+            models.CheckConstraint(
+                check=~Q(origin=F('destination')),
+                name='origin_ne_destination',
+            ),
+            models.CheckConstraint(
+                check=Q(duration__gt=timedelta(minutes=1)),
+                name='duration_gt_1_minute',
+            ),
+            models.CheckConstraint(
+                check=Q(price__gt=Decimal('0')),
+                name='price_gt_0',
+            ),
+        )
+
+    def clean(self):
+        # unique flight check
+        similar_flights = Flight.objects.filter(
+            origin=self.origin,
+            destination=self.destination,
+            duration=self.duration,
+            price=self.price,
+        ).exclude(
+            pk=self.pk
+        )
+
+        if similar_flights.exists():
+            raise ValidationError(
+                'Рейс с такими характеристиками уже существует',
+                code='not_unique_flight'
+            )
+
+        # origin != destination check
+        if self.origin == self.destination:
+            raise ValidationError(
+                'Город отправления и город назначения должны отличаться',
+                code='cities_equal',
+            )
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.origin}-{self.destination}"
