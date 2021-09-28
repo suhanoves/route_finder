31a8b17d98da289bc53c56b28ddff4584303e091
Eugene Suhanov <suhanoves@gmail.com>
2021-09-28 19:36:24 +0300

    add model Flight

--- app/flights/models.py
+++ app/flights/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from cities.models import City
+
+
+class Flight(models.Model):
+    number = models.CharField(
+        verbose_name='номер рейса',
+        max_length=10,
+        unique=True,
+    )
+    duration = models.DurationField(
+        verbose_name='длительность',
+    )
+    price = models.DecimalField(
+        verbose_name='стоимость',
+        max_digits=14,
+        decimal_places=2,
+    )
+    origin = models.ForeignKey(
+        City,
+        on_delete=models.PROTECT,
+        verbose_name='город отправления',
+        related_name='departing_flights',
+        related_query_name='departing_flight',
+    )
+    destination = models.ForeignKey(
+        City,
+        on_delete=models.PROTECT,
+        verbose_name='город назначения',
+        related_name='arriving_flights',
+        related_query_name='arriving_flight',
+    )
+
+    class Meta:
+        verbose_name = 'рейс'
+        verbose_name_plural = 'рейсы'
+
+    def __str__(self):
+        return f"{self.origin}-{self.destination}"
+
+    def get_absolute_url(self):
+        return reverse('flights:flight', kwargs={'pk': self.pk})
