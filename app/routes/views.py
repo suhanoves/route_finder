becbbd0c4e8ddb0ddd808e0ed5d1c96629b5a636
Eugene Suhanov <suhanoves@gmail.com>
2021-09-29 21:37:47 +0300

    add RouteSearchFormView

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView, ListView
+
+from routes.forms import RouteSearchForm
+
+
+class RouteSearchFormView(FormView):
+    form_class = RouteSearchForm
+    template_name = 'routes/route_search.html'
+    success_url = reverse_lazy('routes:found_routes')
+
 
 # Create your views here.
