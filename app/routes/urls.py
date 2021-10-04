f62d5fc6e870c9848632883eb6f282d5be4a3e5e
Eugene Suhanov <suhanoves@gmail.com>
2021-10-04 21:41:19 +0300

    Change Homepage url and view

--- app/routes/urls.py
+++ app/routes/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from routes.views import RouteSearchFormView, FoundRoutesView, RoutesListView
+from routes.views import Homepage, FoundRoutesView, RoutesListView
 
 app_name = 'routes'
 
 urlpatterns = [
-    path('', RouteSearchFormView.as_view(), name='route_search'),
+    path('', Homepage.as_view(), name='homepage'),
     path('found_routes/', FoundRoutesView.as_view(), name='found_routes'),
     path('routes/', RoutesListView.as_view(), name='routes'),
 ]
--- app/routes/views.py
+++ app/routes/views.py
@@ -1,13 +1,19 @@
-from django.urls import reverse_lazy
-from django.views.generic import FormView, ListView
+from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import FormView, FormMixin
 
 from routes.forms import RouteSearchForm
+from routes.services.route_finder import route_finder
 
 
-class RouteSearchFormView(FormView):
+class Homepage(FormMixin, TemplateView):
     form_class = RouteSearchForm
-    template_name = 'routes/route_search.html'
-    success_url = reverse_lazy('routes:found_routes')
+    template_name = 'routes/homepage.html'
 
 
-# Create your views here.
+class FoundRoutesView(FormView):
+    form_class = RouteSearchForm
+    template_name = 'routes/found_routes.html'
+
+    def form_valid(self, form):
+        self.extra_context = route_finder(self.request, form)
+        return super().form_valid(form)
