e5a0b20e46a87d752b572004193d86bd9d990b74
Eugene Suhanov <suhanoves@gmail.com>
2021-10-05 21:10:52 +0300

    Change FoundRoutesView, it renders content now

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, TemplateView
+from django.views.generic import TemplateView
 from django.views.generic.edit import FormView, FormMixin
 
 from routes.forms import RouteSearchForm
@@ -15,5 +15,6 @@ class FoundRoutesView(FormView):
     template_name = 'routes/found_routes.html'
 
     def form_valid(self, form):
-        self.extra_context = route_finder(self.request, form)
-        return super().form_valid(form)
+        routes = route_finder(form)
+        context = {'form': form, 'routes': routes}
+        return super().render_to_response(context)
