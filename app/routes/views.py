aefa154cb38f49c8f5acdc6e50158c1177bfe6cd
Eugene Suhanov <suhanoves@gmail.com>
2021-10-15 21:42:56 +0300

    minor changes in flights and routes app.

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,7 +1,9 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView, TemplateView
-from django.views.generic.edit import FormView, FormMixin
+from django.views.generic.edit import FormView, FormMixin, CreateView
 
 from routes.forms import RouteSearchForm
+from routes.models import Route
 from routes.services.route_finder import route_finder
 
 
@@ -16,8 +18,15 @@ class FoundRoutesView(FormView):
 
     def form_valid(self, form):
         routes = route_finder(form)
+        self.object_list = routes
         context = {'form': form, 'routes': routes}
         return super().render_to_response(context)
+
+
+class CreateRouteView(CreateView):
+    model = Route
+    fields = ('name', 'duration', 'price', 'origin', 'destination', 'flights',)
+    success_url = reverse_lazy('routes:found_routes')
 
 
 class RoutesListView(ListView):
