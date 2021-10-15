1af005caa99b5d8979e97c5ca138a750b463f8bb
Eugene Suhanov <suhanoves@gmail.com>
2021-10-15 17:50:56 +0300

    fix 'is_active_tab' tag if no request.resolver_match

--- app/cities/templatetags/bootstrap_tags.py
+++ app/cities/templatetags/bootstrap_tags.py
@@ -7,14 +7,15 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def is_active_tab(context, *url_pattern_names):
-    request: HttpRequest = context['request']
+    request: HttpRequest = context.get('request')
 
-    app_name = request.resolver_match.app_name
-    url_pattern_name = request.resolver_match.url_name
+    if request.resolver_match:
+        app_name = request.resolver_match.app_name
+        url_pattern_name = request.resolver_match.url_name
 
-    for name in url_pattern_names:
-        if f"{app_name}:{url_pattern_name}" == name:
-            return 'active'
+        for name in url_pattern_names:
+            if f"{app_name}:{url_pattern_name}" == name:
+                return 'active'
     return ''
 
 
